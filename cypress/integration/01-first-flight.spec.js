6562f82ad8b9efd7909a9e6352c278cf6e1b1990
Imre Bartis <imre.bartis@siili.com>
2022-03-17 08:10:36 +0200

    get, contains & should exist tests

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -3,7 +3,13 @@
 describe('Create a New Item', () => {
   beforeEach(() => {
     cy.visit('/jetsetter');
-  })
+  });
 
-  it('should', () => {});
+  it('should have a form', () => {
+    cy.get('form').should('exist');
+  });
+
+  it('should have the words "Add Item"', () => {
+    cy.contains('Add Item');
+  });
 });
